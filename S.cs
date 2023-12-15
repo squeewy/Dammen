2fbd8ee732c34f75afac6fdd8ebc04c3105c67fb
squeewy <206148@student.aventus.nl>
2023-12-15 13:20:03 +0100

    commit C

--- S.cs
+++ S.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+// using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,16 +13,17 @@ public class S : MonoBehaviour
         cs.x /= 2;
         cs.y /= 2;
         float left = (cs.z - size.x) * -1, top = (cs.y - size.y);
-        Color[] colors= new Color[] { color.white, colors.black};
+        Color[] colors= new Color[] { Color.white, Color.black};
         Image drt = D.GetComponent<Image>(), Ci = D.transform.Find("C").GetComponent<Image>();
         for(int i = 0; i < n; i++)
         {
-            if (i % 2 == 0 ){ colors[0] = colors.black; colors[1] = color.white;}
-            else { colors[0] = color.white; colors[1] = color.black;}
+            if (i % 2 == 0 ){ colors[0] = Color.black; colors[1] = Color.white;}
+            else { colors[0] = Color.white; colors[1] = Color.black;}
             for (int j = 0; j < n; j++)
             {
                 drt.color = colors[(((j % 2) == 0) ? 0 : 1)];
-                if (i == (n / 2) - 1 || (n /2) || drt.color == colors.white) C1.enabled = false;
+                if (i == (n / 2) - 1 || (n /2) || drt.color == Color.white) C1.enabled = false;
+                else C1.enabled = true;
                 g[i, j] = Instantiate(D);
                 g[i, j].transform.SetParent(transform.Find("Panel1"));
                 g[i, j].transform.localPosition = new Vector 3(left, top);
